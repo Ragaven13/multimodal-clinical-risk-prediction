bab9a9b76b42010ce6ed2eaef46cdf73a332817b
Anandha Ragaven <ragavenr1301@gmail.com>
2026-04-16 00:41:27 -0400

    Merged clinical notes with ICU labels for text model training

--- src/text/train_text_model.py
+++ src/text/train_text_model.py
@@ -3,24 +3,62 @@ from re import L
 from pathlib import Path
 import pandas as pd
 
-def load_notes(sample_size =10000):
-    print("Loading clinical notes...")
-    path = Path("/Users/anandharagaven/Documents/multimodal-clinical-predictions/data/raw/mimic-iv-note/note/discharge.csv.gz")
 
-    if not path.exists():
-        raise FileNotFoundError(f"File not found: {path}")
+def load_notes(sample_size=10000):
+    print("Loading discharge notes...")
+
+    notes_path = Path("/Users/anandharagaven/Documents/multimodal-clinical-predictions/data/raw/mimic-iv-note/note/discharge.csv.gz")
+
+    if not notes_path.exists():
+        raise FileNotFoundError(f"File not found: {notes_path}")
+
+    df_notes = pd.read_csv(notes_path, compression="gzip")
+
+    print("Notes shape:", df_notes.shape)
+
+    # Sample
+    df_notes = df_notes.sample(n=sample_size, random_state=42)
+
+    return df_notes
+
+def load_labels():
+    print("Loading ICU Labels... ")
+
+    labels_path = Path("/Users/anandharagaven/Documents/multimodal-clinical-predictions/data/processed/final_dataset.csv")
+
+    if not labels_path.exists():
+        raise FileNotFoundError(f"File not found: {labels_path}")
     
-    df = pd.read_csv(path, compression="gzip")
+    df_labels = pd.read_csv(labels_path)
 
-    print("Orginal shape: ", df.shape)
+    print("Labels shape: ", df_labels.shape)
 
-    df = df.sample(n= sample_size, random_state=42)
+    return df_labels
 
-    print("Sampled Size: ", df.shape)
-    print(df.head())
 
-    return df
+def merge_notes_labels(df_notes, df_labels):
+    print("Merging notes with ICU Lables... ")
+    
+    df_notes = df_notes[["subject_id","hadm_id","text"]]
+
+    df_labels = df_labels[["subject_id","hadm_id","icu_admission"]]
+
+    df_merged = pd.merge(
+        df_notes,
+        df_labels,
+        on = ["subject_id","hadm_id"],
+        how = "inner"
+    )
+
+    print("Merged shape: ", df_merged.shape)
+    print(df_merged.head())
+
+    return df_merged
+
+
 
 if __name__ == "__main__":
-    df = load_notes()
+    df_notes = load_notes()
+    df_labels = load_labels()
+    df_final = merge_notes_labels(df_notes,df_labels)
 
